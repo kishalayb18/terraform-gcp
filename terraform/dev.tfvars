b152696454def41664d4a6414fcc5402bb2fb07c
KISHALAY BHATTACHARYA <64585126+kishalayb18@users.noreply.github.com>
2024-11-22 21:25:28 +0530

    refactored vpc (#5)
    * resolved tf push pipeline
    
    * renamed pipeline script
    
    * refactored vpc
    
    * modified op tf
    
    * removed file
    
    * resolved typo
    
    * resolved typo

--- terraform/dev.tfvars
+++ terraform/dev.tfvars
@@ -2,4 +2,5 @@ project_id         = "cloudplay-437100"
 region             = "us-central1"
 vpc_name           = "custom-vpc"
 subnet_names       = ["private-subnet", "public-subnet"]
-subnet_cidr_ranges = ["10.0.0.0/16", "10.0.1.0/16"]
+public_subnet_cidr_ranges = ["10.0.0.170/28"]
+private_subnet_cidr_ranges = ["10.0.0.0/28", "10.0.0.61/28" ]
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,17 @@
-output "vpc_name" {
-  description = "Name of the created VPC"
-  value       = google_compute_network.vpc.name
-}
+# output "vpc_name" {
+#   description = "Name of the created VPC"
+#   value       = google_compute_network.vpc.name
+# }
 
-output "private_subnet_name" {
-  description = "Name of the created private subnet"
-  value       = google_compute_subnetwork.private_subnet.name
-}
+# output "private_subnet_name" {
+#   description = "Name of the created private subnet"
+#   # value       = google_compute_subnetwork.private_subnet.name
+#   value =  google_compute_subnetwork.private_subnet[count.index].name
+  
+# }
 
-output "public_subnet_name" {
-  description = "Name of the created public subnet"
-  value       = google_compute_subnetwork.public_subnet.name
-}
+# output "public_subnet_name" {
+#   description = "Name of the created public subnet"
+#   # value       = google_compute_subnetwork.public_subnet.name
+#   value =  google_compute_subnetwork.public_subnet[count.index].name
+# }
