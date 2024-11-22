3e745b5e04047cec70a272f5b35d25aed8369064
Kishalay Bhattacharya <bhattacharyakishalay@gmail.com>
2024-11-22 21:23:19 +0530

    resolved typo

--- terraform/network.tf
+++ terraform/network.tf
@@ -5,7 +5,7 @@ resource "google_compute_network" "vpc" {
 
 resource "google_compute_subnetwork" "public_subnet" {
   count = length(var.public_subnet_cidr_ranges)
-  name                     = "${var.subnet_names[1]}-${var.region}}-${count.index}"
+  name                     = "${var.subnet_names[1]}-${var.region}-${count.index}"
   region                   = var.region
   network                  = google_compute_network.vpc.name
   ip_cidr_range            = var.public_subnet_cidr_ranges[0]
