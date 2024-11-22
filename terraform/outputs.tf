99afa9036d35918117541963b0b4e51953f62662
Kishalay Bhattacharya <bhattacharyakishalay@gmail.com>
2024-11-22 21:12:13 +0530

    modified op tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,10 +5,13 @@ output "vpc_name" {
 
 output "private_subnet_name" {
   description = "Name of the created private subnet"
-  value       = google_compute_subnetwork.private_subnet.name
+  # value       = google_compute_subnetwork.private_subnet.name
+  value =  google_compute_subnetwork.private_subnet[count.index].name
+  
 }
 
 output "public_subnet_name" {
   description = "Name of the created public subnet"
-  value       = google_compute_subnetwork.public_subnet.name
+  # value       = google_compute_subnetwork.public_subnet.name
+  value =  google_compute_subnetwork.public_subnet[count.index].name
 }
